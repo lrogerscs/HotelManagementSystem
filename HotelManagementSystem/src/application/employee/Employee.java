05ef29a0f5183917c21d25ccb034926d85ce414e
Kyle Hamilton <fremontkdh@gmail.com>
2024-04-26 21:25:49 -0700

    Add files via upload

--- HotelManagementSystem/src/application/employee/Employee.java
+++ HotelManagementSystem/src/application/employee/Employee.java
@@ -3,15 +3,17 @@ package application.employee;
 public class Employee {
 		
 	private int customerId;
-	private int loginId;
+	private int hotelId;
+	private String loginId;
 	private String name;
 	private String title;
 	private String email;
 	private String phoneNumber;
 	private String streetAddress;
 	
-	public Employee(int customerId, int loginId, String name, String title, String email, String phoneNumber, String streetAddress) {
+	public Employee(int customerId, int hotelId, String loginId, String name, String title, String email, String phoneNumber, String streetAddress) {
 		this.customerId = customerId;
+		this.hotelId = hotelId;
 		this.loginId = loginId;
 		this.name = name;
 		this.title = title;
@@ -24,7 +26,11 @@ public class Employee {
        return customerId;
     }
 	
-	public int getLoginId() {
+	public int getHotelId() {
+	   return hotelId;
+	}
+	
+	public String getLoginId() {
 	   return loginId;
 	}
    
