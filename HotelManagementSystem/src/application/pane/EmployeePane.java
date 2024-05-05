81c3c930d3234a3dc1df9930680252d51388f6cc
Lee Rogers <lrogers0941@gmail.com>
2024-05-04 22:56:40 -0700

    Revert "Add files via upload"
    This reverts commit 28bfeb9c628c4cb9255a34905719f34801f5b3bd.

--- HotelManagementSystem/src/application/pane/EmployeePane.java
+++ HotelManagementSystem/src/application/pane/EmployeePane.java
@@ -18,10 +18,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -93,7 +91,7 @@ public class EmployeePane extends VBox {
          
          statement.executeUpdate("delete from Employee where EmployeeID = " + employee.getEmployeeId());
          if (employee.getLoginId() != null)
-            statement.executeUpdate("delete from AuthenticationSystem where LoginID = '" + employee.getLoginId() + "'");
+            statement.executeUpdate("delete from AuthenticationSystem where LoginID = " + employee.getEmployeeId());
          
          connection.close();
          
@@ -107,26 +105,10 @@ public class EmployeePane extends VBox {
          stage.setScene(scene);
          stage.show();
          controller.setHotel(hotel);
-         
-         Alert alert = new Alert(AlertType.INFORMATION);
-         alert.setTitle("Delete Success");
-         alert.setHeaderText(null);
-         alert.setContentText("Employee deleted successfully!");
-         alert.showAndWait();
       } catch (IOException e) {
          e.printStackTrace();
-         Alert alert = new Alert(AlertType.INFORMATION);
-         alert.setTitle("Delete Failure");
-         alert.setHeaderText(null);
-         alert.setContentText(e.getMessage());
-         alert.showAndWait();
       } catch (SQLException e) {
          e.printStackTrace();
-         Alert alert = new Alert(AlertType.INFORMATION);
-         alert.setTitle("Delete Failure");
-         alert.setHeaderText(null);
-         alert.setContentText(e.getMessage());
-         alert.showAndWait();
       }
    }
 }
