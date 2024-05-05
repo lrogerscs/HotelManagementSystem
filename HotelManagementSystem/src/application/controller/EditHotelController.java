89c12f3864505e05619dd13011f2822442601462
Lee Rogers <lrogers0941@gmail.com>
2024-05-04 22:56:31 -0700

    Revert "added alert when add/edit/deleting"
    This reverts commit 0ab642228d1c354e7c57e1d028281c4cab900c49.

--- HotelManagementSystem/src/application/controller/EditHotelController.java
+++ HotelManagementSystem/src/application/controller/EditHotelController.java
@@ -20,8 +20,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 
 public class EditHotelController implements Initializable {
    @FXML
@@ -69,24 +67,13 @@ public class EditHotelController implements Initializable {
          statement.executeUpdate("update Room set HotelID = " + hotelId.getText() 
                + " where HotelID = " + hotel.getHotelId());
          statement.executeUpdate("update Employee set HotelID = " + hotelId.getText() 
-               + " where HotelID = " + hotel.getHotelId());
+               + "where HotelID = " + hotel.getHotelId());
          connection.close();
-         
-         Alert alert = new Alert(AlertType.INFORMATION);
-         alert.setTitle("Update Success");
-         alert.setHeaderText(null);
-         alert.setContentText("Hotel updated successfully!");
-         alert.showAndWait();
          
          // Return to home
          onBackButtonClick(event);
       } catch (Exception e) {
-    	  e.printStackTrace();
-    	  Alert alert = new Alert(AlertType.INFORMATION);
-          alert.setTitle("Update Failure");
-          alert.setHeaderText(null);
-          alert.setContentText(e.getMessage());
-          alert.showAndWait();
+         e.printStackTrace();
       }
    }
    
